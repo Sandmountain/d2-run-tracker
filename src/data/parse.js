43c8d7ab1139303858cf52440435d61a6d9471dc
Viktor Sandberg <sandtain@gmail.com>
2021-10-30 15:39:52 +0200

    update database to contain rarity

--- src/data/parse.js
+++ src/data/parse.js
@@ -17,12 +17,26 @@ const fixName = (name) => {
   return name.replaceAll(' ', '_').replace("'", '').replace('.png', '').toLowerCase();
 };
 
+const getRarity = (set, type) => {
+  console.log(set, type);
+  if (set !== '') return 'set';
+
+  if (type.includes('rune')) {
+    return 'rune';
+  } else if (type.includes('charm')) {
+    return 'magic';
+  } else {
+    return 'unique';
+  }
+};
+
 const arrayOfObj = Object.entries(json).map((e) => ({
   name: e[1].name,
   url: fixName(e[1].name),
   set: e[1].set,
   type: e[1].type,
   reqLvl: e[1].reqLvl,
+  rarity: getRarity(e[1].set, e[1].type),
 }));
 
 FileSystem.writeFile('database.json', JSON.stringify(arrayOfObj), (error) => {
