cb5d7920243bc33257903c5abee6acbb4175f5d6
Viktor Sandberg <sandtain@gmail.com>
2021-11-06 02:01:09 +0100

    Move appbar to component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
 import React, { useState } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import "./App.css";
-
 import MainLayout from "./components/RunLayout/RunLayout.js";
-
-import img from "./components/NavBar/logo.png";
-import { Avatar, Typography, Toolbar, Box, AppBar, Tabs, Tab, IconButton, Tooltip } from "@mui/material";
-import { withStyles } from "@mui/styles";
-
-import AccountCircle from "@mui/icons-material/AccountCircle";
-
-import { Switch, Route, Link, BrowserRouter, Redirect } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
 import SignIn from "./components/SignIn/SignIn";
+import NavBar from "./components/NavBar/NavBar";
+
+import "./App.css";
+import BackgroundImage from "./components/BackgroundImage/BackgroundImage.js";
 
 const theme = createTheme({
   palette: {
     mode: "dark",
     primary: {
-      main: "#7D0B0D",
+      main: "#8f6c32",
     },
     secondary: {
-      main: "#fbce50",
+      main: "#8f6c32",
     },
+    // #fbce50
     info: {
       main: "#ffffff",
     },
   },
 });
-
-const CustomTab = withStyles({
-  selected: {
-    background: "linear-gradient(0deg, rgba(125,11,13,0.2) 0%, rgba(0,212,255,0) 30%)",
-  },
-  disabled: {
-    opacity: 0.3,
-  },
-})(Tab);
 
 function App() {
   const allTabs = ["/", "/holy-grail", "/mule"];
@@ -45,59 +32,8 @@ function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="sticky">
-            <Toolbar variant="dense">
-              <Typography variant="h6" color="inherit" component="div" display="flex" alignItems="center">
-                <Link to={allTabs[0]} className="header-link">
-                  <Avatar alt="Remy Sharp" src={img} style={{ marginRight: "10px" }} />
-                  <span className="diablo-text shadow unique">D2tracker</span>
-                </Link>
-              </Typography>
-              <Box sx={{ marginLeft: "50px" }}>
-                <Route
-                  path="/"
-                  render={({ location }) => (
-                    <>
-                      <Tabs value={location.pathname} centered>
-                        <CustomTab
-                          className="diablo-text shadow"
-                          label="Run Analyze"
-                          value={loggedIn ? allTabs[0] : "/login"} // Hack to not need to add the tab for login
-                          component={Link}
-                          to={allTabs[0]}
-                          disabled={!loggedIn}
-                        />
-                        <CustomTab
-                          className="diablo-text shadow"
-                          label="🛠️ Holy Grail"
-                          value="/holy-grail"
-                          style={{ textDecoration: "line-through" }}
-                          component={Link}
-                          to={allTabs[1]}
-                          disabled
-                        />
-                        <CustomTab
-                          className="diablo-text shadow"
-                          label="🛠️ Mules "
-                          style={{ textDecoration: "line-through" }}
-                          value="/mule"
-                          component={Link}
-                          to={allTabs[2]}
-                          disabled
-                        />
-                      </Tabs>
-                    </>
-                  )}
-                />
-              </Box>
-              <Box sx={{ flexGrow: 1 }} />
-              <IconButton size="small" edge="end" aria-label="account of current user" aria-haspopup="true" color="inherit">
-                <AccountCircle />
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-        </Box>
+        <BackgroundImage></BackgroundImage>
+        <NavBar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
         <Switch>
           <Route exact path={allTabs[0]}>
             {loggedIn ? <MainLayout /> : <Redirect from="/" to={{ pathname: "/login" }} />}
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,83 @@
 import React from "react";
 import img from "./logo.png";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Avatar from "@mui/material/Avatar";
+import { Avatar, Typography, Toolbar, Box, AppBar, Tabs, Tab, IconButton } from "@mui/material";
+import { withStyles } from "@mui/styles";
 
-export default function NavBar() {
+import { Route, Link } from "react-router-dom";
+
+import AccountMenu from "../AccountMenu/AccountMenu";
+
+const CustomTab = withStyles({
+  selected: {
+    background: "linear-gradient(0deg, rgba(143,108,50,0.2) 0%, rgba(0,212,255,0) 20%)",
+  },
+  disabled: {
+    opacity: 0.3,
+  },
+})(Tab);
+const allTabs = ["/", "/holy-grail", "/mule"];
+
+export default function NavBar(props) {
+  const { loggedIn, setLoggedIn } = props;
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
+      <AppBar position="sticky">
         <Toolbar variant="dense">
           <Typography variant="h6" color="inherit" component="div" display="flex" alignItems="center">
-            <Avatar alt="Remy Sharp" src={img} style={{ marginRight: "10px" }} />
-            <span className="diablo-text shadow">Diablo Runtracker</span>
+            <Link to={allTabs[0]} className="header-link">
+              <Avatar alt="Remy Sharp" src={img} style={{ marginRight: "10px" }} />
+              <span className="diablo-text shadow unique">D2tracker</span>
+            </Link>
           </Typography>
+          <Box sx={{ marginLeft: "50px" }}>
+            <Route
+              path="/"
+              render={({ location }) => (
+                <>
+                  <Tabs value={location.pathname} centered>
+                    <CustomTab
+                      className="diablo-text shadow"
+                      label="Run Analyze"
+                      value={loggedIn ? allTabs[0] : "/login"} // Hack to not need to add the tab for login
+                      component={Link}
+                      to={allTabs[0]}
+                      disabled={!loggedIn}
+                    />
+                    <CustomTab
+                      className="diablo-text shadow"
+                      label="🛠️ Holy Grail"
+                      value="/holy-grail"
+                      style={{ textDecoration: "line-through" }}
+                      component={Link}
+                      to={allTabs[1]}
+                      disabled
+                    />
+                    <CustomTab
+                      className="diablo-text shadow"
+                      label="🛠️ Mules "
+                      style={{ textDecoration: "line-through" }}
+                      value="/mule"
+                      component={Link}
+                      to={allTabs[2]}
+                      disabled
+                    />
+                    <CustomTab
+                      className="diablo-text shadow"
+                      label="🛠️ lgogin "
+                      style={{ textDecoration: "line-through" }}
+                      value="/login"
+                      component={Link}
+                      to={"/login"}
+                      disabled
+                    />
+                  </Tabs>
+                </>
+              )}
+            />
+          </Box>
+          <Box sx={{ flexGrow: 1 }} />
+          {loggedIn && <AccountMenu setLoggedIn={setLoggedIn} />}
         </Toolbar>
       </AppBar>
     </Box>
