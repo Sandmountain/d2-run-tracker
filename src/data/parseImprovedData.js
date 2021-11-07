66ed160b9928a93567339cdcf29e065595c4d1be
Viktor Sandberg <sandtain@gmail.com>
2021-11-07 22:53:23 +0100

    Create new database to easier support custom items

--- src/data/parseImprovedData.js
+++ src/data/parseImprovedData.js
@@ -32,7 +32,37 @@ const struct = {
 //   }
 // });
 
+const regex = /\<([^>]+)>/gi;
+
+const regExpAvg = new RegExp(/\([^)]*Avg\)*/g);
+const regExpBase = new RegExp(/\(Base[^)]*\)*/g);
+
 const newData = [];
+const rangeNumbers = /(\d+)-(\d+)/g;
+const regexNumbers = /[\d]+/g;
+
+const createVariedData = (text) => {
+  const varyingValues = text.match(rangeNumbers);
+  if (varyingValues?.length > 1) {
+    console.log(text);
+    return varyingValues.map((value, index) => {
+      let description = text.split(":");
+      if (description[0] !== "Defense") {
+        description = text.replace(/[\d-]+/g, `{0}`);
+      } else {
+        description = `${description[0]}:`;
+      }
+
+      const single = value.match(regexNumbers);
+      return { low: single[0], high: single[1], text: description };
+    });
+  } else if (varyingValues?.length > 0) {
+    //console.log(varyingValues);
+    const single = varyingValues[0].match(regexNumbers);
+    const val = text.replace(/[\d-]+/g, "{0}");
+    return { low: single[0], high: single[1], text: val };
+  }
+};
 
 // @ts-ignore
 newest.forEach((item, index) => {
@@ -52,18 +82,41 @@ newest.forEach((item, index) => {
     };
 
     const requirements = [];
-    const regex = /\<([^>]+)>/gi;
 
     if (item.category !== "runes") {
       // @ts-ignore
       struct.stats = item.stats.reduce((res, stat, idx) => {
         if (stat.charAt(0) !== "<" && stat.charAt(0) !== "(") {
+          const newReq = { requirement: stat, varies: false };
+
           if (stat.includes("Required Level")) {
             struct.level = parseInt(stat.split(":")[1]);
           } else {
-            stat = stat.replace(regex, "");
-            stat = stat.replaceAll("(varies)", "").trim();
-            requirements.push(stat);
+            newReq.requirement = newReq.requirement.replace(regex, "");
+
+            // if (regExpAvg.test(newReq.requirement)) {
+            //   newReq.requirement = newReq.requirement.split(regExpAvg)[0].trim();
+            // }
+
+            // if (regExpBase.test(newReq.requirement)) {
+            //   newReq.requirement = newReq.requirement.split(regExpBase)[0].trim();
+            // }
+
+            if (newReq.requirement.includes("(Base Defense")) {
+              newReq.requirement = newReq.requirement.split(regExpBase)[0].trim();
+            }
+
+            if (newReq.requirement.includes("Avg)")) {
+              newReq.requirement = newReq.requirement.split(regExpAvg)[0].trim();
+            }
+
+            if (newReq.requirement.includes("varies")) {
+              newReq.requirement = newReq.requirement.replaceAll("(varies)", "").trim();
+              newReq.requirement = createVariedData(newReq.requirement);
+              newReq.varies = true;
+            }
+
+            requirements.push(newReq);
           }
         } else if (stat.charAt(0) !== "(") {
           const newDetail = { detail: stat, varies: false };
@@ -72,6 +125,7 @@ newest.forEach((item, index) => {
 
           if (newDetail.detail.includes("varies")) {
             newDetail.detail = newDetail.detail.replaceAll("(varies)", "").trim();
+            newDetail.detail = createVariedData(newDetail.detail);
             newDetail.varies = true;
           }
           res.push(newDetail);
@@ -113,6 +167,6 @@ newest.forEach((item, index) => {
   }
 });
 
-FileSystem.writeFile("data.json", JSON.stringify(newData), (error) => {
+FileSystem.writeFile("testdata.json", JSON.stringify(newData), (error) => {
   if (error) throw error;
 });
