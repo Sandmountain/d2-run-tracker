e3ef284054bdd983bf5e8269874da399505c031c
Viktor Sandberg <sandtain@gmail.com>
2021-11-13 23:49:25 +0100

    Update item dialog to inform of item edit

--- src/components/dialogs/ItemDialog.js
+++ src/components/dialogs/ItemDialog.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { Alert, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 
 import AutoComplete from "../AutoComplete/AutoComplete";
 import { RockDialog } from "../overriden-mui-components/Dialog/Dialog";
@@ -8,10 +8,16 @@ import { RockDialog } from "../overriden-mui-components/Dialog/Dialog";
 export default function ItemDialog(props) {
   const { openNewRunDialog, handleCloseNewRunDialog, setDialogItems, dialogItems } = props;
   return (
-    <RockDialog fullWidth maxWidth="sm" open={openNewRunDialog} onClose={handleCloseNewRunDialog}>
+    <RockDialog fullWidth maxWidth="sm" open={openNewRunDialog}>
       <DialogTitle className="diablo-text">FOUND ANYTHING OF INTEREST?</DialogTitle>
       <DialogContent>
-        <DialogContentText>Search for an item down below, or add a custom one by pressing return.</DialogContentText>
+        <Alert severity="info">
+          Add your item's rolls by hovering it and change the values. This will compare it to your prior finds to see if it is an upgrade.
+        </Alert>
+        <DialogContentText sx={{ marginTop: "15px" }}>
+          Search for an item down below, or add a custom one by pressing return.
+        </DialogContentText>
+
         <AutoComplete setDialogItems={setDialogItems} dialogItems={dialogItems}></AutoComplete>
       </DialogContent>
 
