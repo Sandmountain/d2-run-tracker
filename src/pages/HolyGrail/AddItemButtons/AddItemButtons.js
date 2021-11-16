9c8d5c1d72f431374d827a4bef1e6cd7eb75b3f2
Viktor Sandberg <sandtain@gmail.com>
2021-11-16 23:30:24 +0100

    Add holy grail import as a component

---
+++ src/pages/HolyGrail/AddItemButtons/AddItemButtons.js
@@ -0,0 +1,58 @@
+import { ArrowBack } from "@mui/icons-material";
+import { Button, Fab } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import AutoComplete from "../../../components/AutoComplete/AutoComplete";
+import ReadExcel from "./ReadExcel/ReadExcel";
+
+export default function AddItemButtons(props) {
+  const { itemsToAdd, setItemsToAdd } = props;
+
+  const [showAutoComplete, setShowAutoComplete] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log(itemsToAdd);
+  }, [itemsToAdd]);
+
+  const handleSubmit = () => {
+    console.log("submitted");
+    setItemsToAdd([]);
+  };
+
+  const handleGoBack = () => {
+    setShowAutoComplete(false);
+    setItemsToAdd([]);
+  };
+
+  return (
+    <Box>
+      {!showAutoComplete ? (
+        <Box sx={{ display: "flex", justifyContent: "center", margin: "15px" }}>
+          <Button onClick={() => setShowAutoComplete(true)}>Add Items</Button>
+          <ReadExcel setItemsToAdd={setItemsToAdd} setShowAutoComplete={setShowAutoComplete} />
+        </Box>
+      ) : (
+        <>
+          <Box>
+            <Box sx={{ width: "100%", display: "flex", justifyContent: "end" }}>
+              <Fab color="primary" size="small" sx={{ margin: "15px 15px 0 0" }} variant="contained" onClick={handleGoBack}>
+                <ArrowBack></ArrowBack>
+              </Fab>
+            </Box>
+            <Box sx={{ width: "60%", position: "relative", margin: "0 auto" }}>
+              <AutoComplete handleSubmit={handleSubmit} customItems={false} dialogItems={itemsToAdd} setDialogItems={setItemsToAdd} />
+            </Box>
+          </Box>
+          <Box></Box>
+        </>
+      )}
+      {itemsToAdd.length > 0 && (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Button variant="contained" onClick={handleSubmit}>
+            Add items to your Holy Grail
+          </Button>
+        </Box>
+      )}
+    </Box>
+  );
+}
